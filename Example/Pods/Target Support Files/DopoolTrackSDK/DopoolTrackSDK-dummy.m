4cd65fc66488fe6fbd6b688f3058280923b562a3
piaomiaoatian667 <piaomiaoatian@yeah.com>
2019-11-14 17:07:09 +0800

    创建上报组件

---
+++ Example/Pods/Target Support Files/DopoolTrackSDK/DopoolTrackSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DopoolTrackSDK : NSObject
+@end
+@implementation PodsDummy_DopoolTrackSDK
+@end
---
+++ Example/Pods/Target Support Files/DopoolTrackSDK/DopoolTrackSDK-umbrella.h
@@ -0,0 +1,31 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DopBase64.h"
+#import "DopCheckUDID.h"
+#import "DopGlobalDefinition.h"
+#import "DopoolReachability.h"
+#import "DopTrack.h"
+#import "DopTrackConst.h"
+#import "DopTrackLib.h"
+#import "DopUDID.h"
+#import "Dop_Device_Checker.h"
+#import "NSData+DopGZipAdditions.h"
+#import "NSDate+DopBJZone.h"
+#import "NSString+DopEncoding.h"
+#import "NSString+DopMD5Addition.h"
+#import "NSString+DopNull.h"
+#import "UIDevice+DopIdentifierAddition.h"
+
+FOUNDATION_EXPORT double DopoolTrackSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char DopoolTrackSDKVersionString[];
+
